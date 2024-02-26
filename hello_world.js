dc0ecde2e03651b50d24cee1491fe6f4ba60903d
Muhammad Faisal Peerzada <66348157+mohammadfaisalpirzada@users.noreply.github.com>
2024-02-26 23:53:38 +0500

    Add files via upload

---
+++ hello_world.js
@@ -0,0 +1,6 @@
+// hello world program
+console.log("Hello World");
+var myname = "Faisal";
+console.log(myname + "1");
+myname = "Peerzada";
+console.log(myname);
---
+++ hello_world.ts
@@ -0,0 +1,6 @@
+// hello world program
+console.log("Hello World");
+let myname: string = "Faisal"
+console.log (myname + "1")
+myname = "Peerzada"
+console.log(myname)
